feb955bd90cf46bad4c41e27ad322b76ab025c16
Shahriyorbek <shakhr1yorbek1@gmail.com>
2023-01-11 17:25:55 +0500

    dsfd

---
+++ lib/bottom/Likes_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Likes_page extends StatefulWidget {
+  const Likes_page({Key? key}) : super(key: key);
+
+  @override
+  State<Likes_page> createState() => _Likes_pageState();
+}
+
+class _Likes_pageState extends State<Likes_page> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
